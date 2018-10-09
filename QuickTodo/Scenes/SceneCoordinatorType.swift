afa83077a74326a37b757d4c021821983f1c3fde
Little-Captain <littlecaptain@foxmail.com>
2018-10-09 14:53:48 +0800

    调整代码格式

--- QuickTodo/Scenes/SceneCoordinatorType.swift
+++ QuickTodo/Scenes/SceneCoordinatorType.swift
@@ -24,18 +24,22 @@ import UIKit
 import RxSwift
 
 protocol SceneCoordinatorType {
-  /// transition to another scene
-  @discardableResult
-  func transition(to scene: Scene, type: SceneTransitionType) -> Completable
-
-  /// pop scene from navigation stack or dismiss current modal
-  @discardableResult
-  func pop(animated: Bool) -> Completable
+    
+    /// transition to another scene
+    @discardableResult
+    func transition(to scene: Scene, type: SceneTransitionType) -> Completable
+    
+    /// pop scene from navigation stack or dismiss current modal
+    @discardableResult
+    func pop(animated: Bool) -> Completable
+    
 }
 
 extension SceneCoordinatorType {
-  @discardableResult
-  func pop() -> Completable {
-    return pop(animated: true)
-  }
+    
+    @discardableResult
+    func pop() -> Completable {
+        return pop(animated: true)
+    }
+    
 }
--- QuickTodo/Scenes/SceneTransitionType.swift
+++ QuickTodo/Scenes/SceneTransitionType.swift
@@ -23,10 +23,12 @@
 import Foundation
 
 enum SceneTransitionType {
-  // you can extend this to add animated transition types,
-  // interactive transitions and even child view controllers!
-
-  case root       // make view controller the root view controller
-  case push       // push view controller to navigation stack
-  case modal      // present view controller modally
+    
+    // you can extend this to add animated transition types,
+    // interactive transitions and even child view controllers!
+    
+    case root       // make view controller the root view controller
+    case push       // push view controller to navigation stack
+    case modal      // present view controller modally
+    
 }
